7be2a3bbe31ef81a7866e3d93faab63f355d8091
ruqaiyae <ruqaiyaelectricwala@gmail.com>
2025-04-03 18:19:21 +0000

    Resized the comments input

--- client/src/Pages/Memories/Image.tsx
+++ client/src/Pages/Memories/Image.tsx
@@ -97,15 +97,15 @@ export function Image() {
       marginRight="0"
       text="A single photo can hold a thousand memories."
       isLoading={isLoading}>
-      <div className="flex flex-wrap md:flex-nowrap md:justify-center">
-        <div className="md:w-[40%] mx-5 md:mx-20">
+      <div className="flex flex-wrap md:flex-nowrap justify-center">
+        <div className="w-[60%] md:w-[40%] mx-5 md:mx-20">
           <div className="relative inline-block">
             <img
               src={image?.imageUrl}
               className="object-contain border-[1.5px] md:border-2 border-[#654A2F] rounded-lg"
             />
-            <div className="flex justify-between content-center mt-1 md:mt-3 w-full">
-              <div className="flex items-center">
+            <div className="flex justify-between items-end md:mt-3 w-full">
+              <div className="flex items-end">
                 {user?.userId === ownerId ? (
                   <Link
                     to={`/family/${familyId}/dashboard/images/${imageId}/edit`}>
@@ -129,7 +129,7 @@ export function Image() {
             </div>
           </div>
         </div>
-        <div className="w-[50%]">
+        <div className="w-[60%] md:w-[50%]">
           <p className="text-[#654A2F] text-[12px] md:text-[20px] mt-5">
             Comments:
           </p>
@@ -139,7 +139,7 @@ export function Image() {
                 placeholder="Add your comment"
                 onChange={(e) => setValue(e.target.value)}
                 value={value}
-                className="border md:border-2 focus:border-2 md:focus:border-3 focus:outline-none border-[#654A2F] rounded md:rounded-md  md:p-4 md:h-8 w-[600px] mt-1 text-[#654A2F] text-[10px] md:text-[17px]"
+                className="border md:border-2 focus:border-2 md:focus:border-3 focus:outline-none border-[#654A2F] rounded md:rounded-md  md:p-4 md:h-8 w-55 md:w-[600px] mt-1 text-[#654A2F] text-[10px] md:text-[17px]"
               />
             </label>
           </form>
@@ -147,7 +147,7 @@ export function Image() {
             {comments?.map((comment) => (
               <li
                 key={comment.commentsId}
-                className="text-[#654A2F] text-[10px] md:text-[15px] mt-1 flex justify-between w-[600px]">
+                className="text-[#654A2F] text-[10px] md:text-[15px] mt-1 flex justify-between md:w-[600px]">
                 <p>
                   <span className="font-medium italic">{comment.author}</span>:{' '}
                   {comment.comment}
